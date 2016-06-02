960f072a63ceab60a6875e4ec52fb5ac4cdcbeb7
waning1995 <a937515012@live.com>
2016-06-02 20:09:19 +0800

    使用VS提交到GitHub

---
+++ NewRepo/NewRepo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是一个新的仓库");
+
+            Console.ReadLine();
+        }
+    }
+}
